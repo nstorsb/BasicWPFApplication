0b4134145192b1cc29253c183aab985a55c7229f
DESKTOP-5NBM43C\Usuario <nstorsb@gmail.com>
2024-07-27 01:52:28 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace BasicWPFApplication;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindow = new MainWindow();
+        MainWindow.Show();
+        base.OnStartup(e);
+    }
+}
